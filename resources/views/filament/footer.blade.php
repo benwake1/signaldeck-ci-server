5eaa1859c49187f68a30991e0a877c1c784d21dd
Ben Wake <benwake1@outlook.com>
2026-03-14 14:14:22 +0000

    removed v- from version

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="px-6 py-4 text-xs text-center text-gray-400 dark:text-gray-500 border-t border-gray-100 dark:border-white/5">
     &copy; {{ date('Y') }} {{ config('brand.legal_name') ?: config('brand.name') }}. All rights reserved.
-    <span class="block mt-0.5">v-{{ config('brand.version') }}</span>
+    <span class="block mt-0.5">{{ config('brand.version') }}</span>
 </div>
